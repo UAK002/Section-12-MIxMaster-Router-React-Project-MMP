47f9f7e9301be7f77c0a76842202f3ec4789908b
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-08-27 04:14:44 +0530

    366. Nested Pages - More Complex Example

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,12 @@
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 const About = () => {
   return (
     <div>
       <h1>About</h1>
-      <Link to="/">Home Page</Link>
-      <a href="https://johnsmilga.com/">best web dev courses</a>
+      {/* <Link to="/">Home Page</Link> */}
+      {/* <a href="https://johnsmilga.com/">best web dev courses</a> */}
+      <Outlet />
     </div>
   );
 };
