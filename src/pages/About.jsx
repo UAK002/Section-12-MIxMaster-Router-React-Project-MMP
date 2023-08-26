5098e566a9979e0f462ec101eaeb3408ebe64315
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-08-27 00:32:35 +0530

    364. Link Component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1>About</h1>
+      <Link to="/">Home Page</Link>
+      <a href="https://johnsmilga.com/">best web dev courses</a>
+    </div>
+  );
 };
 export default About;
