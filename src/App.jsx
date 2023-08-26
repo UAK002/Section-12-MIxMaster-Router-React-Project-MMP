439561b1fe64bafccbaec794ed4b2f1e0f069f59
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-08-26 23:44:51 +0530

    363. Setup Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import About from './pages/About';
+
+import {
+  About,
+  Cocktail,
+  Error,
+  HomeLayout,
+  Landing,
+  Newsletter,
+} from './pages';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <h2>home page</h2>,
+    // element: <h2>home page</h2>,
+    element: <HomeLayout />,
   },
   {
     path: '/about',
-    element: (
-      <div>
-        <h2>about page</h2>,
-      </div>
-    ),
+    // element: (
+    //   <div>
+    //     <h2>about page</h2>,
+    //   </div>
+    // ),
+    element: <About />,
   },
 ]);
 
