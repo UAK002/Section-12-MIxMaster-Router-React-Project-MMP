dfe8d511a0f060995b0fa50150e4106bdb7dc675
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-08-31 02:36:18 +0530

    385. Single Cocktail - Render

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -5,19 +5,62 @@ import Wrapper from '../assets/wrappers/CocktailPage';
 const singleCocktailUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
 
-// export const loader = async (data) => {
 export const loader = async ({ params }) => {
-  // console.log(data);
   const { id } = params;
-  // const response = await axios.get(`${singleCocktailUrl}${id}`);
   const { data } = await axios.get(`${singleCocktailUrl}${id}`);
-  // console.log(response);
-  // return null;
+  console.log(data);
   return { id, data };
 };
 
 const Cocktail = () => {
   const { id, data } = useLoaderData();
-  return <div>Cocktail</div>;
+
+  const singleDrink = data.drinks[0];
+  // console.log(singleDrink);
+
+  const {
+    strDrink: name,
+    strDrinkThumb: image,
+    strAlcoholic: info,
+    strCategory: category,
+    strGlass: glass,
+    strInstructions: instructions,
+  } = singleDrink;
+
+  return (
+    <Wrapper>
+      <header>
+        <Link to="/" className="btn">
+          back home
+        </Link>
+        <h3>{name}</h3>
+      </header>
+      <div className="drink">
+        <img src={image} alt={name} className="img" />
+        <div className="drink-info">
+          <p>
+            <span className="drink-data">name :</span>
+            {name}
+          </p>
+          <p>
+            <span className="drink-data">category :</span>
+            {category}
+          </p>
+          <p>
+            <span className="drink-data">info :</span>
+            {info}
+          </p>
+          <p>
+            <span className="drink-data">glass :</span>
+            {glass}
+          </p>
+          <p>
+            <span className="drink-data">instructions :</span>
+            {instructions}
+          </p>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 export default Cocktail;
